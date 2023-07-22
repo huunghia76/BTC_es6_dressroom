530bcbf681b92416dae4520a6fb5946b01d713e0
Nghia Hoang <nghiahoangas@gmail.com>
2023-07-22 17:00:50 +0700

    commit 22/7/2023

--- controllers/main.js
+++ controllers/main.js
@@ -9,18 +9,14 @@ const renderNavPills = () => {
     API.API().then((data) => {
         // sử dụng destructuring
         const { navPills, tabPanes } = data;
-
         navPills.map((resNav, key) => {
-
             const { tabName, showName, type } = resNav;
             if (key == 0) {
                 tab += `
                 <li class="nav-item" role="presentation">
                     <button class="nav-link active " id="${type}" data-idtab="${type}" data-bs-toggle="tab" data-bs-target="#${type}-pane"
                         type="button" role="tab" aria-controls="${type}-pane" aria-selected="true">${showName}</button>
-                </li>
-                    `
-
+                </li>`
                 content += `
                         <div class="tab-pane fade show active" id="${type}-pane" role="tabpanel" aria-labelledby="${type}"
                         tabindex="0">`
@@ -29,15 +25,12 @@ const renderNavPills = () => {
                 <li class="nav-item" role="presentation">
                     <button class="nav-link " id="${type}" data-idtab="${type}" data-bs-toggle="tab" data-bs-target="#${type}-pane"
                         type="button" role="tab" aria-controls="${type}-pane" aria-selected="true">${showName}</button>
-                </li>
-                    `
+                </li>`
                 content += `
                         <div class="tab-pane fade " id="${type}-pane" role="tabpanel" aria-labelledby="${type}"
                         tabindex="0">`
             }
-
             tabPanes.map((res) => {
-                // debugger
                 // sử dụng destructuring
                 const { id, type, name, desc, imgSrc_jpg, imgSrc_png } = res;
                 if (resNav.type == type) {
@@ -45,23 +38,16 @@ const renderNavPills = () => {
                                     <img class="product-img" width="120px" src="${imgSrc_jpg}" alt="" />
                                     <p> ${name}</p>
                                     <button onclick="handleSetClothes('${type}','${imgSrc_png}' )">Thử đồ</button>
-                                </div>
-                                        `
+                                </div> `
                 }
             })
             content += `</div>`
         })
-
         getElement('#myTab').innerHTML = tab
         getElement('#myTabContent').innerHTML = content
-
     })
-
 }
 renderNavPills()
-
 window.handleSetClothes = (type, src) => {
-
     getElement(`.${type}`).style.background = `url(${src}) 0% 0% / 100%`
-    
 }
